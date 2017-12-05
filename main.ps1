7de5973ee0b2ec96d03fd9dca20f6847c4c81905
yschgroup <32958487+YSCHGroup@users.noreply.github.com>
2017-12-05 17:26:08 +0100

    Updates some stuff
    still not compleatly done yet tho...

--- main.ps1
+++ main.ps1
@@ -1,3 +1,4 @@
+##  Functions   ##
 function new-translation-line {
     Write-Host "`nWord: " -f Yellow -NoNewline; $newglossaryword = read-host;
     Write-Host "Translation: " -f Yellow -NoNewline; $newglossarytranslation = read-host;
@@ -23,7 +24,7 @@ function do-glossary([string]$glossary, [string]$mode) {
                     $word = $word_translation[0]
                     $translation = $word_translation[1]
             
-                    Write-Host "Translate '$word': " -f Yellow -NoNewline; $usertranslation = Read-Host;
+                    Write-Host "Translate '$word' : " -f Yellow -NoNewline; $usertranslation = Read-Host;
                     if (-not ($usertranslation.ToLower() -eq $translation)) {
                         Write-Host "Wrong! Correct translation is '" -f Red -NoNewline; Write-Host $translation -f Yellow -NoNewline; Write-Host "'..." -f Red;
                         $alltranslations += 1;
@@ -42,7 +43,7 @@ function do-glossary([string]$glossary, [string]$mode) {
                     $translation = $word_translation[0]
                     $word = $word_translation[1]
             
-                    Write-Host "Translate '$word': " -f Yellow -NoNewline; $usertranslation = Read-Host;
+                    Write-Host "Translate '$word' : " -f Yellow -NoNewline; $usertranslation = Read-Host;
                     if (-not ($usertranslation.ToLower() -eq $translation)) {
                         Write-Host "Wrong! Correct translation is '" -f Red -NoNewline; Write-Host $translation -f Yellow -NoNewline; Write-Host "'..." -f Red;
                         $alltranslations += 1;
@@ -58,11 +59,49 @@ function do-glossary([string]$glossary, [string]$mode) {
             if ($atline -gt 1) {
                 if($_ -match $regex){
                     $word_translation = $_.ToString().Split("-")
+                    $translation = $word_translation[0]
+                    $word = $word_translation[1]
+            
+                    Write-Host "Translate '$word' : " -f Yellow -NoNewline; $usertranslation = Read-Host;
+                    if (-not ($usertranslation.ToLower() -eq $translation)) {
+                        Write-Host "Wrong! Correct translation is '" -f Red -NoNewline; Write-Host $translation -f Yellow -NoNewline; Write-Host "'..." -f Red;
+                        $alltranslations += 1;
+                    }else {
+                        Write-Host "Correct!" -f Green;
+                        $correcttranslations += 1;
+                        $alltranslations += 1;
+                    }
+                }
+            }
+        }elseif ($mode -eq 4) {
+            $atline += 1;
+            if ($atline -gt 1) {
+                if($_ -match $regex){
+                    $word_translation = $_.ToString().Split("-")
+                    $translation = $word_translation[1]
+                    $word = $word_translation[0]
+            
+                    Write-Host "Translate '$word' : " -f Yellow -NoNewline; $usertranslation = Read-Host;
+                    if (-not ($usertranslation.ToLower() -eq $translation)) {
+                        Write-Host "Wrong! Correct translation is '" -f Red -NoNewline; Write-Host $translation -f Yellow -NoNewline; Write-Host "'..." -f Red;
+                        $alltranslations += 1;
+                    }else {
+                        Write-Host "Correct!" -f Green;
+                        $correcttranslations += 1;
+                        $alltranslations += 1;
+                    }
+                }
+            }
+        }elseif ($mode -eq 5) {
+            $atline += 1;
+            if ($atline -gt 1) {
+                if($_ -match $regex){
+                    $word_translation = $_.ToString().Split("-")
                     $word = $word_translation[0]
                     $translation = $word_translation[1]
                     
                     # Get Examples of Sentences from thesaurus.com
-                    Write-Host "`n - Example Context for '" -f DarkGray -NoNewline; Write-Host "$word" -f Yellow -NoNewline; Write-Host "':" -f DarkGray;
+                    Write-Host "`n - Example Context for '" -f DarkGray -NoNewline; Write-Host "$word" -f Yellow -NoNewline; Write-Host "' ('" -f DarkGray -NoNewline; Write-Host "$translation" -f Yellow -NoNewline; Write-Host "') :" -f DarkGray;
                     try {
                         $website = Invoke-WebRequest -Uri http://www.thesaurus.com/browse/$word+?s=t -ErrorAction stop;
                         $examples = $website.AllElements | Where {$_.TagName -eq "p"} -ErrorAction stop;
@@ -70,7 +109,7 @@ function do-glossary([string]$glossary, [string]$mode) {
                         $totalexamplestried += 1;
                     }
                     catch {
-                        Write-Host "Example not found..." -f Red;
+                        Write-Host " Example not found..." -f Red;
                         $examplesfailed += 1;
                         $totalexamplestried += 1;
                     }
@@ -83,13 +122,13 @@ function do-glossary([string]$glossary, [string]$mode) {
         }
     }
 
-    if ($mode -lt 3) {
+    if ($mode -lt 5) {
         $score = [math]::Round($correcttranslations*100/$alltranslations)
-        if ($score -lt 50) { Write-Host "`nAlmost! You knew only $score% of the words..." -f Red }
-        if ($score -eq 50) { Write-Host "`nDecent! You knew 50% of the words." -f yellow }
-        if ($score -ge 50 -and $score -lt 100) { Write-Host "`nGood! You knew $score% of the words!.." -f yellow }
-        if ($score -eq 100) { Write-Host "`nGreat! You knew 100% of the words!" -f green }
-    }elseif ($mode -eq 3) {
+        if ($score -lt 50) { Write-Host "`nAlmost! You knew only $score% of the words..." "($correcttranslations/$alltranslations)" -f Red }
+        if ($score -eq 50) { Write-Host "`nDecent! You knew 50% of the words." "($correcttranslations/$alltranslations)" -f yellow }
+        if ($score -ge 50 -and $score -lt 100) { Write-Host "`nGood! You knew $score% of the words!.." "($correcttranslations/$alltranslations)" -f yellow }
+        if ($score -eq 100) { Write-Host "`nGreat! You knew 100% (All) of the words!" "($correcttranslations/$alltranslations)" -f green }
+    }elseif ($mode -eq 5) {
         $fails = [math]::Round($examplesfailed/$totalexamplestried)
         if ($fails -eq 1) { Write-Host "`nAll attempts failed... Are you sure you have an valid internet connection?" -f Red }
     }
@@ -99,6 +138,9 @@ function do-glossary([string]$glossary, [string]$mode) {
         do-glossary $glossary $mode
     }
 }
+
+
+##  Interface  ##
 
 cls
 Write-Host "-= Glossarys =-`n" -f Yellow;
@@ -144,13 +186,18 @@ if ($choice.ToLower() -eq "new") {
 
 }else {
     if (Test-Path "glossarys\$choice.txt") {
-        Write-Host "Modes:" -f DarkGray;
-        Write-Host "1 - Words to Translations" -f DarkGray;
-        Write-Host "2 - Translations to Words" -f DarkGray;
-        Write-Host "3 - Read all words in a context (Require Internet and English Words Only)  (Under construction...)" -f DarkGray;
+        Write-Host "`nModes:" -f darkGray;
+        Write-Host "1 - Words to Translations" -f white;
+        Write-Host "2 - Translations to Words" -f white;
+        Write-Host "3 - Words to Translations" -f white -NoNewline; Write-Host " (Randomized order/Unsorted)" -f DarkGray;
+        Write-Host "4 - Translations to Words" -f white -NoNewline; Write-Host " (Randomized order/Unsorted)" -f DarkGray;
+        Write-Host "5 - Search for the words in a context" -f white -NoNewline; Write-Host " (Require Internet and English Words Only)" -f DarkGray;
         Write-Host "Select: " -f Yellow -NoNewline; $glosmode = Read-Host;
-        if ($glosmode -le 3) {
+        if ($glosmode -le 5 -and $glosmode -ne 0) {
             do-glossary $choice $glosmode
+        }else {
+            Write-Host "Invalid selection. Please pick a number from the list above!" -f Red;
+            $pause = Read-Host;
         }
     }
 }
